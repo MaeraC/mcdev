b628c95dd98c3817bc24a061bd268ea07ded6e32
Maera <baeitsmae@gmail.com>
2023-07-05 21:53:22 +0200

    images added

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Logo() {
     return (
         <Link to="/" className="logo">
-            <h1>Maera <span className="span3">Studio</span></h1>
+            <h1>MC <span className="span3">Studio</span></h1>
             <span className="span2"></span>
         </Link>
     )
