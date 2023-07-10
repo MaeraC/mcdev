6b427eb4d3901c1fa45cc1d0472d53d19a9e1c3c
Maera <baeitsmae@gmail.com>
2023-07-10 19:59:06 +0200

    media queries

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,6 @@ function Logo() {
     return (
         <Link to="/" className="logo">
             <h1>MC <span className="span3">Studio</span></h1>
-            <span className="span2"></span>
         </Link>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,60 @@
 import { Link } from "react-router-dom"
 import Logo from "./Logo"
-
+import closeBurger from "../assets/close.png"
+import { useState, useEffect } from "react"
 
 function Nav() {
+    const [showMenu, setShowMenu] = useState(true)
+    const [showMenuBurger , setMenuBurger] = useState(false)
+
+    function openMenuBurger() {
+        setMenuBurger(true)
+    }
+
+    useEffect(() => {
+        const handleResize = () => {
+        if (window.innerWidth <= 1024) {
+            setShowMenu(false)
+        } 
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+ 
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+    }, [])
+
     return (
         <nav className="nav">
             <Logo />
             <ul>
-                <li className="link"><Link to="#services" reloadDocument>Services</Link></li>
-                <li className="link"><Link to="#avantages" reloadDocument>Avantages</Link></li>
-                <li className="link"><Link to="#deroulement" reloadDocument>Déroulement</Link></li>
-                <li className="link"><Link to="#realisations" reloadDocument>Réalisations</Link></li>
-                <li className="link"><Link to="#contact" reloadDocument>Contact</Link></li>
+                {showMenu && (
+                    <>
+                    <li className="link"><Link to="#services" reloadDocument>Services</Link></li>
+                    <li className="link"><Link to="#avantages" reloadDocument>Avantages</Link></li>
+                    <li className="link"><Link to="#deroulement" reloadDocument>Déroulement</Link></li>
+                    <li className="link"><Link to="#realisations" reloadDocument>Réalisations</Link></li>
+                    <li className="link"><Link to="#contact" reloadDocument>Contact</Link></li>
+                    </>
+                )}
                 <li><Link to="#devis" className="button" reloadDocument>Obtenir un devis</Link></li>
+                
+                {!showMenu && (
+                    <button className='open-burger button' onClick={openMenuBurger}>Menu</button>
+                )}
+
+                {showMenuBurger && (
+                    <li className="menu2">
+                        <li className="link"><Link to="#services" reloadDocument>Services</Link></li>
+                        <li className="link"><Link to="#avantages" reloadDocument>Avantages</Link></li>
+                        <li className="link"><Link to="#deroulement" reloadDocument>Déroulement</Link></li>
+                        <li className="link"><Link to="#realisations" reloadDocument>Réalisations</Link></li>
+                        <li className="link"><Link to="#contact" reloadDocument>Contact</Link></li>
+                        <img className="close-menu" onClick={() => setMenuBurger(false)} src={closeBurger} alt="fermer le menu" />
+                    </li>
+                )}
             </ul>
         </nav>
     )
