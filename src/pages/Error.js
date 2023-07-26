b11b5e6ee59a09cdad4af831c06e3bbc35d416b1
Maera <baeitsmae@gmail.com>
2023-07-26 20:48:29 +0200

    Error page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 function Error() {
     return (
-        <section>Error</section>
+        <section className="error">
+            <span>404 Error</span>
+            <p>Oups... Cette page n'existe pas !</p>
+            <Link to="/">Retourner à la page d'accueil</Link>
+        </section>
     )
 }
 
